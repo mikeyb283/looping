2c0c6b21c8607d4cc89b4f5b9561386787fdb8af
Michael Balling <mballing@students.mchenry.edu>
2018-01-27 19:45:30 -0600

    Initial commit

---
+++ src/Looping.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Looping {
+  public static void main(String[] args){
+
+    int userChoice;
+    Scanner userIn = new Scanner(System.in);
+
+    do{
+      System.out.println("Welcome to Mike's Electronics Store");
+      System.out.println("**");
+      System.out.println("1) Laptop");
+      System.out.println("2) Tablet");
+      System.out.println("3) Smart Phone");
+      System.out.println("4) EXIT");
+      System.out.println("**");
+      System.out.println("Select an option from the menu above");
+      userChoice = userIn.nextInt();
+    }
+    while (userChoice < 1 || userChoice > 4 );
+
+    switch (userChoice) {
+      case 1:
+        System.out.println("You chose to buy a Laptop the price is $500.");
+        break;
+      case 2:
+        System.out.println("You chose to buy a Tablet the price is $400.");
+        break;
+      case 3:
+        System.out.println("You chose to buy a Smart phone the price is $300.");
+        break;
+      case 4:
+        System.out.println("System exited...");
+        break;
+      default:
+        System.out.println("Invalid choice, try again");
+        break;
+    }
+  }
+}
